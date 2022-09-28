73b9ef5f4d000aa832dda6f34c14d677c6d46eb3
Lucas Sahota <lucaswade4@gmail.com>
2022-09-28 16:05:06 -0700

    Added function to test if objects are equal and print a message to console

---
+++ lotide/assertObjectsEqual.js
@@ -0,0 +1,52 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } 
+  for (let item of Object.keys(object1)) {
+    if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
+      return eqArrays(object1[item], object2[item])
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  } return true;
+}
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+}
+
+const obj1 = {
+  a: "1",
+  b: "2"
+}
+
+const obj2 = {
+  b: "2",
+  a: "1"
+}
+
+const obj3 = {
+  a: "4",
+}
+
+assertObjectsEqual(obj1, obj2)
+assertObjectsEqual(obj1, obj3)
