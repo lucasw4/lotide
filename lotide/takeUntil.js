43401e081817dba28c1021077852d5b033a61741
Lucas Sahota <lucaswade4@gmail.com>
2022-10-13 14:06:28 -0700

    added refactoring and commenting

--- lotide/takeUntil.js
+++ lotide/takeUntil.js
@@ -1,25 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2, result) {
-  if (eqArrays(array1, array2) === result) {
-    return `🟢🟢 Assertion Passed: The result of comparing ${array1} && ${array2} === ${result}`;
-  } else {
-    return `🔴🔴 Assertion Failed: $The result of comparing ${array1} && ${array2} !== ${result}`;
-  }
-};
-
 const takeUntil = function(array, callBack) {
+  // Returns a new array that takes every element out of a given array until the callbackfunction parameters are met.
   let results = [];
   for (let i of array) {
     if (!callBack(i)) {
@@ -30,11 +10,4 @@ const takeUntil = function(array, callBack) {
   }
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-
-console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2], true));
-console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"], true));
+module.exports = takeUntil;
--- lotide/without.js
+++ lotide/without.js
@@ -1,31 +1,12 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (array1, array2, result) {
-  if (eqArrays(array1, array2) === result) {
-    return `🟢🟢 Assertion Passed: The result of comparing ${[array1]} && ${array2} === ${result}`
-  } else {
-    return `🔴🔴 Assertion Failed: The result of comparing ${array1} && ${array2} !== ${result}`
-  }
-}
-
-
 const without = function (source, itemsToRemove) {
-  let sourceRemoved = [];
-  for (let i = 0; i < source.length; i++) {
-    if (source[i] !== itemsToRemove) {
-      sourceRemoved.push(source[i]);
+  // given a source array and an itemsToRemove array, returns a new array with all itemsToRemove removed.
+  const sourceRemoved = []
+  source.forEach(ele => {
+    if(!itemsToRemove.includes(ele)) {
+      sourceRemoved.push(ele)
     }
-  }
-  return sourceRemoved;
+  });
+  return sourceRemoved
 }
+
+console.log(without([1,2,3,9,5,8],[2,8]),[1,3,9,5])
