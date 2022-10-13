ab86f01914c729ff86eed8633748d2c42f282a87
Lucas Sahota <lucaswade4@gmail.com>
2022-10-12 18:02:40 -0700

    added tests and refactoring for eqArrays.js

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢 Assertion Passed: ${[actual]} === ${expected}`);
-  } else {
-    console.log(`🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,3 +10,5 @@ const eqArrays = function (array1, array2) {
   }
   return true;
 }
+
+module.exports = eqArrays;
