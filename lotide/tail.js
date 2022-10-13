f84aa59cb5deeab1d1bda033c406de3a8c59c48e
Lucas Sahota <lucaswade4@gmail.com>
2022-10-12 17:58:32 -0700

    Added test for tail.js and refactored tail.js

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,11 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢 Assertion Passed: ${[actual]} === ${expected}`);
-  } else {
-    console.log(`🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(array) {
   return array.slice(1);
 };
+
+module.exports = tail
