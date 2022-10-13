4ea6d54f0277f4d6cf889b26ebfc8499c13e620a
Lucas Sahota <lucaswade4@gmail.com>
2022-10-13 13:46:25 -0700

    Fixing and refactoring countOnly.js, adding test file for countOnly.js

--- lotide/countOnly.js
+++ lotide/countOnly.js
@@ -1,23 +1,18 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢 Assertion Passed: ${[actual]} === ${expected}`);
-  } else {
-    console.log(`🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countOnly = function(array, items) {
   const results = {};
-
+// Counts each item from an items array that appears in a given array and returns an object with each count.
   for (const item of array) {
-    if (items[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
+    for (const checkItem of items) {
+      if (item === checkItem) {
+        if(!results[item]) {
+          results[item] = 1;
+        } else {
+          results[item] += 1
+        }
       }
     }
   }
   return results;
-
 };
+console.log(countOnly([3,3,3], [3]))
+module.exports = countOnly;
--- lotide/test/countOnlyTest.js
+++ lotide/test/countOnlyTest.js
@@ -1,2 +1,8 @@
 const _ = require('../index')
 const assert = require('chai').assert
+
+describe('#countOnly', () => {
+  it('returns object with key 3 and count 3', () => {
+    assert.equal(_.countOnly([3,3,3], [3])[3], 3)
+  })
+})
